832e593b07ef77768da4580e7dccdac2785048f9
Hee Seung Yun <yoonihg@gmail.com>
2023-03-24 10:39:27 +0900

    file changed

--- comp.py
+++ comp.py
@@ -8,11 +8,11 @@ from transformers import (
     AutoModelForSeq2SeqLM,
     AutoTokenizer,
     DataCollatorForSeq2Seq,
-    Seq2SeqTrainingArguments, 
+    Seq2SeqTrainingArguments,
     Seq2SeqTrainer)
-    
+
 from datasets import load_metric, Dataset
-from dacon_submit_api import dacon_submit_api 
+from dacon_submit_api import dacon_submit_api
 
 import numpy as np
 import pandas as pd
@@ -48,7 +48,7 @@ TEST_SOURCE = os.path.join(DIR, "test.json")
 
 with open(TRAIN_SOURCE) as f:
     TRAIN_DATA = json.loads(f.read())
-    
+
 with open(TEST_SOURCE) as f:
     TEST_DATA = json.loads(f.read())
 
@@ -92,14 +92,14 @@ def preprocess_function(batch):
     prefix = "summarize: "
     inputs = [prefix + doc for doc in batch["total"]]
     inputs = tokenizer(inputs, padding="max_length",
-                        truncation=True,
-                        max_length=encoder_max_length)
+                       truncation=True,
+                       max_length=encoder_max_length)
 
     # Setup the tokenizer for targets
     with tokenizer.as_target_tokenizer():
         outputs = tokenizer(batch["summary"], padding="max_length",
-                        truncation=True,
-                        max_length=encoder_max_length)
+                            truncation=True,
+                            max_length=encoder_max_length)
 
     batch["input_ids"] = inputs.input_ids
     batch["attention_mask"] = inputs.attention_mask
@@ -107,9 +107,9 @@ def preprocess_function(batch):
     batch["decoder_attention_mask"] = outputs.attention_mask
     batch["labels"] = outputs.input_ids.copy()
     batch["labels"] = [
-        [-100 if token == tokenizer.pad_token_id \
-            else token for token in labels] \
-                for labels in batch["labels"]]
+        [-100 if token == tokenizer.pad_token_id
+            else token for token in labels]
+        for labels in batch["labels"]]
     return batch
 
 
@@ -121,19 +121,19 @@ def preprocess_function(batch):
 #     # Replace -100 in the labels as we can't decode them.
 #     labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
 #     decoded_labels = tokenizer(labels, skip_special_tokens=True)
-    
+
 #     # Rouge expects a newline after each sentence
 #     decoded_preds = ["\n".join(kkma.sentences(pred.strip())) for pred in decoded_preds]
 #     decoded_labels = ["\n".join(kkma.sentences(label.strip())) for label in decoded_labels]
-    
+
 #     result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
 #     # Extract a few results
 #     result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
-    
+
 #     # Add mean generated length
 #     prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
 #     result["gen_len"] = np.mean(prediction_lens)
-    
+
 #     return {k: round(v, 4) for k, v in result.items()}
 
 train_data = Dataset.from_pandas(df_train)
@@ -145,13 +145,13 @@ train_data = train_data.map(
     batched=True,
     batch_size=batch_size,
     remove_columns=[
-        'uid', 'title', 'region', 
+        'uid', 'title', 'region',
         'context', 'summary', 'total']
 )
 train_data.set_format(
-    type="torch", 
+    type="torch",
     columns=[
-        "input_ids", "attention_mask", "decoder_input_ids", 
+        "input_ids", "attention_mask", "decoder_input_ids",
         "decoder_attention_mask", "labels"],
 )
 
@@ -161,70 +161,71 @@ val_data = val_data.map(
     batched=True,
     batch_size=batch_size,
     remove_columns=[
-        'uid', 'title', 'region', 
+        'uid', 'title', 'region',
         'context', 'summary', 'total']
 )
 val_data.set_format(
-    type="torch", 
+    type="torch",
     columns=[
-        "input_ids", "attention_mask", "decoder_input_ids", 
+        "input_ids", "attention_mask", "decoder_input_ids",
         "decoder_attention_mask", "labels"],
 )
 
 
 training_args = Seq2SeqTrainingArguments(
-                    save_strategy="epoch",
-                    evaluation_strategy="epoch",
-                    num_train_epochs=epochs,
-                    logging_strategy="epoch",
-                    do_train=True,
-                    do_eval=True,
-                    per_device_train_batch_size=batch_size,
-                    per_device_eval_batch_size=batch_size,
-                    output_dir='./log',
-                    load_best_model_at_end=True,
-                    learning_rate=learning_rate,
-                    disable_tqdm=True,
-                    )
+    save_strategy="epoch",
+    evaluation_strategy="epoch",
+    num_train_epochs=epochs,
+    logging_strategy="epoch",
+    do_train=True,
+    do_eval=True,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    output_dir='./log',
+    load_best_model_at_end=True,
+    learning_rate=learning_rate,
+    disable_tqdm=True,
+)
 
 data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
 
 
 trainer = Seq2SeqTrainer(
-            model=model,
-            args=training_args,
-            data_collator=data_collator,
-            train_dataset=train_data,
-            eval_dataset=val_data,
-            tokenizer=tokenizer,
-            optimizers='AdamW',
-            # compute_metrics=compute_metrics,
-            )
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=train_data,
+    eval_dataset=val_data,
+    tokenizer=tokenizer,
+    optimizers='AdamW',
+    # compute_metrics=compute_metrics,
+)
 
 trainer.train()
 
 model.to("cuda")
 
+
 def generate_summary(batch):
     prefix = "summarize: "
     inputs = [prefix + doc for doc in batch["total"]]
     inputs = tokenizer(inputs,
-                        padding="max_length",
-                        truncation=True,
-                        max_length=encoder_max_length,
-                        return_tensors="pt")
+                       padding="max_length",
+                       truncation=True,
+                       max_length=encoder_max_length,
+                       return_tensors="pt")
 
     input_ids = inputs.input_ids.to("cuda")
     attention_mask = inputs.attention_mask.to("cuda")
 
     outputs = model.generate(
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                max_length=decoder_max_length,)
+        input_ids=input_ids,
+        attention_mask=attention_mask,
+        max_length=decoder_max_length,)
 
     output_str = tokenizer.batch_decode(
-                                outputs,
-                                skip_special_tokens=True)
+        outputs,
+        skip_special_tokens=True)
 
     batch["summary"] = output_str
 
@@ -232,9 +233,9 @@ def generate_summary(batch):
 
 
 results = test_data.map(
-                    generate_summary,
-                    batched=True,
-                    batch_size=batch_size)
+    generate_summary,
+    batched=True,
+    batch_size=batch_size)
 
 pred_str = results["total"]
 label_str = results["summary"]
@@ -244,9 +245,9 @@ sample_submission['summary'] = label_str
 sample_submission.to_csv('sub1.csv', index=False)
 
 result = dacon_submit_api.post_submission_file(
-    'sub1.csv', 
-    '63af198a705c8b8bcb5c3f0bcd84b1c8e8435c9da10875843871a770c269d0e6', 
+    'sub1.csv',
+    '63af198a705c8b8bcb5c3f0bcd84b1c8e8435c9da10875843871a770c269d0e6',
     '235813',
-    '영락태왕', 
+    '영락태왕',
     'submission'
 )
